549a097a545a8e37c6931d98dc75b8f2308505ff
JulianoPassing <julianosalm@gmail.com>
2025-09-05 15:04:15 -0300

    att
    att

--- carros.js
+++ carros.js
@@ -5,5 +5,12 @@ const carrosData = [
         "Valor": "R$ 100.000.000.000,00",
         "Categoria": "Especial",
         "Imagem": "https://i.imgur.com/Wf7bGAO.png"
+    },
+    {
+        "Carro": "Boeing 747",
+        "Ano": "2020",
+        "Valor": "R$ 500.000.000,00",
+        "Categoria": "Aeronaves",
+        "Imagem": "https://i.imgur.com/Wf7bGAO.png"
     }
 ];
