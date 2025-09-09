f6805936d903d69f303bc494997426c0685b27a8
JulianoPassing <julianosalm@gmail.com>
2025-09-09 13:13:01 -0300

    Atualização automática da tabela - 09/09/2025, 13:12:56

--- carros.js
+++ carros.js
@@ -1,1 +1,9 @@
-const carrosData = [];
+const carrosData = [
+    {
+        "Carro": "Furd Ranger Raptor",
+        "Ano": "2019",
+        "Valor": "80 Moedas",
+        "Categoria": "Off-road",
+        "Imagem": "https://i.imgur.com/Wf7bGAO.png"
+    }
+];
