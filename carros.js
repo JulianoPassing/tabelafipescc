5ab06425d599b8e4730110757345f77ffaedd658
JulianoPassing <julianosalm@gmail.com>
2025-09-09 13:13:31 -0300

    Atualização automática da tabela - 09/09/2025, 13:13:26

--- carros.js
+++ carros.js
@@ -5,5 +5,12 @@ const carrosData = [
         "Valor": "80 Moedas",
         "Categoria": "Off-road",
         "Imagem": "https://i.imgur.com/Wf7bGAO.png"
+    },
+    {
+        "Carro": "Hunda Civic FL5",
+        "Ano": "2024",
+        "Valor": "120 Moedas",
+        "Categoria": "Sedan",
+        "Imagem": "https://i.imgur.com/Wf7bGAO.png"
     }
 ];
