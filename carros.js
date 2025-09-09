3148831b94b64ec1682b83e6274f11453fc60b2d
JulianoPassing <julianosalm@gmail.com>
2025-09-09 13:07:20 -0300

    Atualização automática da tabela - 09/09/2025, 13:07:15

--- carros.js
+++ carros.js
@@ -1,9 +1,1 @@
-const carrosData = [
-    {
-        "Carro": "SCC",
-        "Ano": "2025",
-        "Valor": "R$ 100.000.000.000,00",
-        "Categoria": "Especial",
-        "Imagem": "https://i.imgur.com/Wf7bGAO.png"
-    }
-];
+const carrosData = [];
