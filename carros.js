511a1990d5accba879c4647897117686dce7ca6c
JulianoPassing <julianosalm@gmail.com>
2025-10-28 05:58:27 -0300

    Atualização automática da tabela - 28/10/2025, 05:58:25

--- carros.js
+++ carros.js
@@ -572,5 +572,12 @@ const carrosData = [
         "Valor": "R$ 437.990",
         "Categoria": "Coupe",
         "Imagem": "https://i.imgur.com/Wf7bGAO.png"
+    },
+    {
+        "Carro": "Emperor SC300",
+        "Ano": "2000",
+        "Valor": "60 Moedas",
+        "Categoria": "Sedan",
+        "Imagem": "https://i.imgur.com/Wf7bGAO.png"
     }
 ];
